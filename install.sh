4f3e2b9f9a60767fea87012643cfdf3989a31972
Jeff Emershaw <me@jeffemershaw.com>
2021-07-02 16:26:27 +0000

    Fixing enable script for runit

--- install.sh
+++ install.sh
@@ -883,7 +883,7 @@ runit_start() {
     local try=0
     until $SUDO sv restart ${SYSTEM_NAME} > /dev/null;do
       try=$(expr $try + 1)
-      [ $try > 3 ] && exit 1
+      [ "$try" -gt 5 ] && exit 1 || sleep 3
     done
 }
 
