0fd52edc7310162343142a432778ca9bc4260566
Jeff Emershaw <me@jeffemershaw.com>
2021-07-02 17:10:52 +0000

    Fixing

--- install.sh
+++ install.sh
@@ -908,7 +908,7 @@ service_enable_and_start() {
 
     [ "${HAS_SYSTEMD}" = true ] && systemd_start
     [ "${HAS_OPENRC}" = true ] && openrc_start
-    [ "${HAS_RUNIT}" = true ] && runit_start
+    # [ "${HAS_RUNIT}" = true ] && runit_start
     return 0
 }
 
